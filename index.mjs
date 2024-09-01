eb5c8a4813c8a0f2a46fc40c06697a833334e895
DevCodes <office@devcodes.ro>
2024-09-01 12:14:57 +0300

    add express to serve the html file and send the 503 status code

---
+++ index.mjs
@@ -0,0 +1,15 @@
+import express from "express";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(503).sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
